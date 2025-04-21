9ddec623b3e01159330bd4cd1842c6c1c68a7afd
Inti <147012070+IntiCerda@users.noreply.github.com>
2025-04-21 17:50:59 -0400

    eldiavlo

--- internal/handlers/graphql.go
+++ internal/handlers/graphql.go
@@ -5,7 +5,7 @@ import (
 	"github.com/graphql-go/handler"
 )
 
-// GraphQLHandler adapta el handler de graphql-go para usarlo con Gin
+// GraphQLHandler adapta el handler de graphql-go para usarlo con Gin (???????????????????????)
 func GraphQLHandler(h *handler.Handler) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		h.ServeHTTP(c.Writer, c.Request)
